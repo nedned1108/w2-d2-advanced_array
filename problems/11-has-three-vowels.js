80270e3379d48fb27a845e5e2dc4f9e558ee3c46
nednguyen <rpmshockwave@gmail.com>
2022-10-07 00:20:21 -0400

    finished 15

---
+++ problems/11-has-three-vowels.js
@@ -0,0 +1,44 @@
+/*
+Write a function `hasThreeVowels` that accepts a string as an argument.
+The function should return a boolean indicating whether or not the string
+contains at least three different vowels.
+
+Solve this using Array's `forEach()`, `map()`, `filter()` **OR** `reduce()`
+methods.
+
+Examples:
+
+console.log(hasThreeVowels('delicious'));       //  true
+console.log(hasThreeVowels('bootcamp prep'));   //  true
+console.log(hasThreeVowels('bootcamp'));        //  false
+console.log(hasThreeVowels('dog'));             //  false
+console.log(hasThreeVowels('go home'));         //  false
+
+*/
+
+let hasThreeVowels = function(string) {
+    let vowels = "aeiou";
+    let currentVowels = [];
+    let chars = string.split("");
+    chars.forEach((char) => {
+        if (vowels.includes(char) && currentVowels.indexOf(char) === -1) {
+            currentVowels.push(char);
+        }
+    })
+    if (currentVowels.length >= 3) return true;
+    else return false;
+};
+
+console.log(hasThreeVowels('delicious'));       //  true
+console.log(hasThreeVowels('bootcamp prep'));   //  true
+console.log(hasThreeVowels('bootcamp'));        //  false
+console.log(hasThreeVowels('dog'));             //  false
+console.log(hasThreeVowels('go home'));         //  false
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+
+try {
+    module.exports = hasThreeVowels;
+} catch (e) {
+    module.exports = null;
+}
